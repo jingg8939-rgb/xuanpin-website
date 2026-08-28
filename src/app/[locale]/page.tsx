50c2186f7852735374681e55794acaed792386dd
Xuanpin Deploy <deploy@xuanpinglobal.com>
2026-08-28 16:49:38 +0800

    fix: make product names clickable with underline and pointer cursor on homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -207,7 +207,7 @@ export default async function HomePage({
                       <svg className="w-4 h-4 mt-1 text-[#C8102E] shrink-0" fill="currentColor" viewBox="0 0 20 20">
                         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                       </svg>
-                      <Link href={linkHref} className="hover:text-[#C8102E] transition-colors">
+                      <Link href={linkHref} className="hover:text-[#C8102E] hover:underline transition-colors cursor-pointer">
                         {itemName}
                       </Link>
                     </li>
@@ -232,7 +232,7 @@ export default async function HomePage({
                       <svg className="w-4 h-4 mt-1 text-[#C8102E] shrink-0" fill="currentColor" viewBox="0 0 20 20">
                         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                       </svg>
-                      <Link href={linkHref} className="hover:text-[#C8102E] transition-colors">
+                      <Link href={linkHref} className="hover:text-[#C8102E] hover:underline transition-colors cursor-pointer">
                         {itemName}
                       </Link>
                     </li>
@@ -257,7 +257,7 @@ export default async function HomePage({
                       <svg className="w-4 h-4 mt-1 text-[#C8102E] shrink-0" fill="currentColor" viewBox="0 0 20 20">
                         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                       </svg>
-                      <Link href={linkHref} className="hover:text-[#C8102E] transition-colors">
+                      <Link href={linkHref} className="hover:text-[#C8102E] hover:underline transition-colors cursor-pointer">
                         {itemName}
                       </Link>
                     </li>
